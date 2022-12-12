9a1e18bf98d2f2d98f77c48f4c93242b265ae416
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2022-12-12 16:40:41 +0200

    Inital commit

---
+++ build/index.js
@@ -0,0 +1,7 @@
+import express from 'express';
+const PORT = 3000;
+const app = express();
+app.listen(PORT, () => {
+    console.log(`Server started at port:${PORT}`);
+});
+//# sourceMappingURL=index.js.map
